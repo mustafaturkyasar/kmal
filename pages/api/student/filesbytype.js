b6cac7e3dc583255176eaaab56dfbbda97360513
mustafa türkyaşar <48827510+mustafaturkyasar@users.noreply.github.com>
2020-12-08 12:46:49 +0300

    Update filesbytype.js

--- pages/api/student/filesbytype.js
+++ pages/api/student/filesbytype.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
                 // make sure that any items are correctly URL encoded in the connection string
                 await sql.connect('mssql://portal:22166973Td!*@176.98.40.103/SINIFTANIM')
                 const result = await sql.query`select  m.ogrenciid,m.resimyol,d.aciklama from ogrenciResim m,dosyaTipi d where m.tipid = d.id
-                 and m.ogrenciid =  ${id} and d.id = ${type} `
+                 and m.ogrenciid =  ${id} and d.id = ${type} order by m.id desc`
                 res.send(result.recordset)
             } catch (err) {
                 console.log(err)
